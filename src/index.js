fd0bda997edda96aec75cb5976ac65bec02b9ee9
Agbor Kima <agborkima1@gmail.com>
2024-07-04 18:36:18 -0500

    can select a project and add a todo it

--- src/index.js
+++ src/index.js
@@ -1,38 +1,56 @@
 class Todo {
-    constructor() {
-
-        constructor(title, description, dueDate, priority); {
+        constructor(title, description, dueDate, priority) {
             this.title = title,
-                this.description = description,
-                this.dueDate = dueDate,
-                this.priority = priority
-
+            this.description = description,
+            this.dueDate = dueDate,
+            this.priority = priority
         }
-    }
 }
 
 class Project {
-    constructor(name, todos, selected){
+    constructor(name) {
         this.name = name
-        //an array of todos for the given project
-        this.todos = todos
+        this.todos = []
         this.selected = false
     }
-    setSelected(){
+    setSelected() {
         this.selected = true
     }
-    
-    addTodo(todo){
+
+    addTodo(title, description, dueDate, priority) {
+        const todo = new Todo(title, description, dueDate, priority)
         this.todos.push(todo)
     }
 }
 
 const allProjects = []
 
+const createNewTodo = () => {
+
+    for (let i = 0; i < allProjects.length; i++) {
+        if (allProjects[i].selected === true) {
+
+            allProjects[i].addTodo(prompt('Title'), prompt('Description'), prompt('Due Date'), prompt('Priority'))
+        }
+    }
+
+}
+
+const showTodos = () => {
+    allProjects.map(project => {
+        project.selected ?
+            project.todos.forEach(todo => {
+                const singleTodo = document.createElement('div')
+                const item = document.createElement('h4')
+                item.textContent = todo.title
+                todosDiv.append(item)
+            }) : project
+    })
+
+}
 
 
-
-function createNewProject(){
+function createNewProject() {
     //change to some type of form
     const name = prompt('enter project name')
     const project = new Project(name, [])
@@ -49,21 +67,31 @@ const addProjectBtn = document.getElementById('addProject')
 addProjectBtn.addEventListener('click', () => createNewProject())
 
 const addTodoBtn = document.getElementById('addTodo')
-addTodoBtn.addEventListener('click', () => createNewTodo())
+addTodoBtn.addEventListener('click', () => {
+
+    createNewTodo()
+    console.log(allProjects)
+
+})
+
+const todosDiv = document.getElementById('todos-div')
 
 const projectsDiv = document.getElementById('projects-div')
 projectsDiv.addEventListener('click', (e) => {
-    //when clicking on a project, set its selected value to true and others to false
     allProjects.forEach(proj => {
         proj.selected = false
     })
 
     // console.log(e.target.closest('h4').textContent)
     const clickedProject = e.target.closest('h4')
-    for(let i = 0; i < allProjects.length; i++){
-        if(allProjects[i].name === clickedProject.textContent){
+    for (let i = 0; i < allProjects.length; i++) {
+        if (allProjects[i].name === clickedProject.textContent) {
             allProjects[i].setSelected()
+            clickedProject.setAttribute('style', 'background-color: green')
+        }
 
+        else if(clickedProject.textContent !== allProjects[i].name){
+            clickedProject.setAttribute('style', 'background-color: azure;')
         }
     }
     console.log(allProjects)
